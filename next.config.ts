48723eb7cce3d6c57bdb9cdbb2b965a5f47c308e
roguemaps <jesse.van@gmail.com>
2026-04-17 10:26:03 -0700

    added no jekyll and minor tweaks

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/itspersonal/' : '',
+  basePath: isProd ? '/itspersonal' : '',
+  output: 'export'
 };
 
 export default nextConfig;
